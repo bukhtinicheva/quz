56fff7fd902cd8d5ff0d4a290d0e3f5b06151e60
missheroin <Angelinak554@gmail.com>
2022-03-13 21:21:24 +0300

    current text-consol version

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ import random
 def format_osn(stroka):
     a = []
     for i in range(len(stroka)):
-        a.append(str(stroka[i])[2:-2])
+        a.append(str(stroka[i])[1:-2])
     a = str(a)
-    a = a.replace("[", "")
-    a = a.replace("]", "")
-    a = a.replace("'", "")
-    a = a.replace(",", "")
+    a = a.replace("[", '')
+    a = a.replace("]", '')
+    a = a.replace("'", '')
+    a = a.replace(",", '')
     a = list(a)
-    return "".join(a[1:-1])
+    return ''.join(a[1:-1])
+
+def format_cor(stroka):
+    a = []
+    for i in range(len(stroka)):
+        a.append(str(stroka[i])[1:-2])
+    a = str(a)
+    a = a.replace("[", '')
+    a = a.replace("]", '')
+    a = a.replace("'", '')
+    a = a[1:-1]
+    a = a.split(", ")
+    return a
 
 def format_ans(stroka):
     a = []
@@ -22,20 +34,23 @@ def format_ans(stroka):
     a = a.replace("]", "")
     a = a.replace("'", "")
     a = a.replace(",", "")
-    a = a.replace(" ", "")
+    a = a[1:]
     a = list(a)
-    return ", ".join(a[1:-1])
+    for i in range(len(a)):
+        if a[i].isupper() or a[i].isdigit():
+            a[i-1] = ', '
+    return "".join(a[:-1])
 
 flag = True
 level = 1
 while flag and level <= 5:
     print(f'Уровень {level}')
     a = []
-    connection = sqlite3.connect("proba.db")
+    connection = sqlite3.connect("quizz.db")
     cursor = connection.cursor()
     count_quest = cursor.execute(f"""SELECT id FROM level_{level}""").fetchall()
     lenght = [i for i in range(1, len(count_quest) + 1)]
-    number_s = random.sample(lenght, 2)
+    number_s = random.sample(lenght, 4)
     count = 0
     fl = True
     while fl and count < 2:
@@ -44,19 +59,20 @@ while flag and level <= 5:
         ansers = format_ans(cursor.execute(f"""SELECT ans_1, ans_2, ans_3, ans_4 FROM level_{level} WHERE id = {number_s[count - 1]}""").fetchall())
         print(f'Внимание, вопрос: {question}')
         print(f'Варианты ответа: {ansers}')
-        correct = format_osn(cursor.execute(f"""SELECT correct_ans FROM level_{level} WHERE id = {number_s[count - 1]}""").fetchall())
+        correct = format_cor(cursor.execute(f"""SELECT correct_ans FROM level_{level} WHERE id = {number_s[count - 1]}""").fetchall())
         anser = input()
-        if anser == correct:
-            print('Правильный ответ')
+        if anser in correct:
+            print('Правильный ответ\n')
         else:
             print('Неправильный ответ. Вы проиграли')
             fl = False
     if fl == False:
         flag = False
-    if count == 2:
-        print(f'Поздравляю, вы прошли уровень {level}!')
-        print('-----------------------------------------')
-        count = 0
-        level += 1
-if level == 5:
-    print('Поздравляю! Вы прошли викторину!')
+    if flag:
+        if count == 2:
+            print(f'Поздравляю, вы прошли уровень {level}!')
+            print('-----------------------------------------')
+            count = 0
+            if level == 5:
+                print('Поздравляю! Вы прошли викторину!')
+            level += 1
